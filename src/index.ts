5c4e79b1cb7fe493343b5628b3ac445908bfd03d
ja153903 <jaimeabbariao@gmail.com>
2023-02-06 23:57:17 -0500

    feat: adding simple feature

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 import Express from 'express'
 import dotenv from 'dotenv'
 
+import { postRouter } from '@/features/posts'
+
 dotenv.config()
 
 const app = Express()
 const port = process.env.PORT || 3000
+
+app.use(postRouter)
 
 app.listen(port, () => {
 	console.log(`Server is running on port ${port}`)
