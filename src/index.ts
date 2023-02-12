7acabc75356b99bb9c97c29addfd712b334b34ea
ja153903 <jaimeabbariao@gmail.com>
2023-02-12 00:54:04 -0500

    feat: updating with module-alias

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,25 @@
+/** This is necessary for Node to pick up path aliases */
+import 'module-alias/register'
+
+/** Initialize environment variables */
 import dotenv from 'dotenv'
+dotenv.config()
+
 import express from 'express'
 
 import { postRouter } from '@/features/posts'
 
-dotenv.config()
-
 const app = express()
 const port = process.env.PORT || 3000
 
+/** Middleware */
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+/** API Routers */
 app.use('/api/v1/posts', postRouter)
 
+/** Listen on Port */
 app.listen(port, () => {
 	console.log(`Server is running on port ${port}`)
 })
