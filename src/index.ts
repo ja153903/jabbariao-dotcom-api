45d3ce710a82cc56dc1cf2d880cc8689fdddc59c
ja153903 <jaimeabbariao@gmail.com>
2023-02-24 18:02:40 -0500

    feat: removing comments

--- src/index.ts
+++ src/index.ts
@@ -16,14 +16,11 @@ import { postRouter } from '@/features/posts'
 const app = express()
 const port = process.env.PORT || 3000
 
-/** Middleware */
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-/** API Routers */
 app.use('/api/v1/posts', postRouter)
 
-/** Listen on Port */
 app.listen(port, () => {
 	console.log(`Currently running in ${process.env.NODE_ENV} mode`)
 	console.log(`Server is running on port ${port}`)
